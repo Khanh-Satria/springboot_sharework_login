f151180433ea63a4320c8017d0d80e84cddd761a
unknown <phamtuankhanh028@gmail.com>
2023-05-14 21:28:13 +0700

    Update Render

--- src/main/java/com/springboot/sharework/controller/AuthContronller.java
+++ src/main/java/com/springboot/sharework/controller/AuthContronller.java
@@ -3,6 +3,7 @@ package com.springboot.sharework.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ import com.springboot.sharework.payload.SignInRequest;
 import com.springboot.sharework.payload.SignInResponse;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
 public class AuthContronller {
 
 	@Value("${firebase.apiKey}")
@@ -27,6 +28,15 @@ public class AuthContronller {
 	
 	public AuthContronller(AuthenServiceImpl authenService) {
 		this.authenService = authenService;
+	}
+	
+	
+	@GetMapping
+	public ResponseEntity<String> index(){
+		
+		String response = "SHAREWORK HELLO!!";
+		
+		return new  ResponseEntity<>(response, HttpStatus.OK);
 	}
 	
 	@PostMapping("/register")
